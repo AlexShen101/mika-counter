d8cf82091cf0b1288c5f4264c847fcd586944ded
alexander shen <alexander.shen@gmail.com>
2024-04-21 11:31:07 -0400

    changed base url in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://alexshen101.github.io/mika-counter/'
+  base: 'https://fun.mika-counter.alexshen.com.de'
 })
